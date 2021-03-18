b4200a57caff5b1126beef8dfe9fe05dd9c07593
akeneo <contact@akeneo.com>
2021-03-18 12:53:32 +0000

    Extract from https://github.com/akeneo/pim-community-dev/commit/aa6045a5e2904616051f67f7ab4cc3b58330da09 With commits: RAC-556: first version of the MediaLinkInput component (#14091) RAC-556: first version of the MediaLinkInput component (#14091)

--- lib/components/Input/MediaFileInput/MediaFileInput.d.ts
+++ lib/components/Input/MediaFileInput/MediaFileInput.d.ts
@@ -2,7 +2,6 @@ import React from 'react';
 import { Override } from '../../../shared';
 import { InputProps } from '../InputProps';
 import { FileInfo } from './FileInfo';
-declare type PreviewType = 'preview' | 'thumbnail';
 declare type MediaFileInputProps = Override<Override<React.InputHTMLAttributes<HTMLInputElement>, InputProps<FileInfo | null>>, ({
     readOnly: true;
 } | {
@@ -10,16 +9,11 @@ declare type MediaFileInputProps = Override<Override<React.InputHTMLAttributes<H
     onChange: (newValue: FileInfo | null) => void;
 }) & {
     value: FileInfo | null;
-    previewer: (value: FileInfo, type: PreviewType) => string;
+    thumbnailUrl: string | null;
     uploader: (file: File, onProgress: (ratio: number) => void) => Promise<FileInfo>;
-    downloader: (value: FileInfo) => string;
     placeholder?: string;
     uploadingLabel: string;
-    downloadLabel: string;
     clearTitle: string;
-    fullscreenTitle: string;
-    fullscreenLabel?: string;
-    closeTitle: string;
     uploadErrorLabel: string;
     size?: 'default' | 'small';
     invalid?: boolean;
