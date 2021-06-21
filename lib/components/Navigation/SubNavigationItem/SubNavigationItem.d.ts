e7d3f1578e7c47c7c906c225bf526b64b70c42a9
akeneo <contact@akeneo.com>
2021-06-21 16:17:30 +0000

    Extract from https://github.com/akeneo/pim-community-dev/commit/e91d62f6e206e81cdbd9d9104bafbea548ac2eb4 With commits: Merge pull request #14648 from akeneo/CXP-714 Merge branch 'master' into CXP-714 Merge branch 'master' into CXP-714 Merge pull request #14759 from akeneo/CXP-714-visual-update Update visual tests for branch CXP-714 CXP-714: fix coverage of tabBar component CXP-714 Added SubNavigationItem to the design system Merge pull request #14648 from akeneo/CXP-714

---
+++ lib/components/Navigation/SubNavigationItem/SubNavigationItem.d.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+declare const SubNavigationItem: React.ForwardRefExoticComponent<Omit<React.HTMLAttributes<HTMLAnchorElement>, "href" | "children" | "disabled" | "active"> & {
+    children?: React.ReactNode;
+    active?: boolean | undefined;
+    disabled?: boolean | undefined;
+    href?: string | undefined;
+} & React.RefAttributes<HTMLAnchorElement>>;
+export { SubNavigationItem };
