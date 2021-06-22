77ac65b051da912072406d4d67fa49c39bd86b7d
akeneo <contact@akeneo.com>
2021-06-22 12:29:20 +0000

    Extract from https://github.com/akeneo/pim-community-dev/commit/91961342e7fc665d237e5d2be5ad0eace1f67704 With commits: Merge pull request #14702 from akeneo/CXP-715 CXP-715: add SubNavigationPanel component to DSM Merge pull request #14702 from akeneo/CXP-715

---
+++ lib/components/Navigation/SubNavigationPanel/SubNavigationPanel.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+declare type SubNavigationPanelProps = {
+    children?: React.ReactNode;
+    isOpen?: boolean;
+};
+declare const SubNavigationPanel: React.ForwardRefExoticComponent<SubNavigationPanelProps & React.RefAttributes<HTMLDivElement>>;
+export { SubNavigationPanel };
