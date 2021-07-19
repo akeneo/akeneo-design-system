68ab0cfa538e78ce0bcd5bb88db2956d81055960
akeneo <contact@akeneo.com>
2021-07-19 11:20:35 +0000

    Extract from https://github.com/akeneo/pim-community-dev/commit/34b172e6d302a578016b78a8dcab8643e948a898 With commits: PIM-9973: Fix Asset attribute media type dropdown being hidden (#14989) PIM-9973: Fix Asset attribute media type dropdown being hidden (#14989)

--- lib/components/Input/SelectInput/Overlay/Overlay.d.ts
+++ lib/components/Input/SelectInput/Overlay/Overlay.d.ts
@@ -1,10 +1,11 @@
-import React, { ReactNode, RefObject } from 'react';
+import React, { HTMLAttributes, ReactNode, RefObject } from 'react';
 import { VerticalPosition } from '../../../../hooks';
-declare type OverlayProps = {
+import { Override } from '../../../../shared';
+declare type OverlayProps = Override<HTMLAttributes<HTMLDivElement>, {
     verticalPosition?: VerticalPosition;
     onClose: () => void;
     children: ReactNode;
     parentRef?: RefObject<HTMLDivElement>;
-};
-declare const Overlay: ({ verticalPosition, parentRef, onClose, children }: OverlayProps) => React.ReactPortal;
+}>;
+declare const Overlay: ({ verticalPosition, parentRef, onClose, children, ...rest }: OverlayProps) => React.ReactPortal;
 export { Overlay };
