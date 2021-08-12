da04747f0837d560800aeccbccd4387a5793713d
akeneo <contact@akeneo.com>
2021-08-12 09:09:03 +0000

    Extract from https://github.com/akeneo/pim-community-dev/commit/2748d60dab8ba696903ccf5e1780e5691c3604ad With commits: Merge pull request #14888 from akeneo/PLG-352 PLG-388: Fix performance issue with the opening of the menu (#15009) Merge branch 'master' into PLG-352 PLG-352: fix linter issue Merge branch 'master' into PLG-352 Merge pull request #15026 from akeneo/PLG-392 Merge branch 'PLG-352' into PLG-392 PLG-392: Prettier Merge branch 'master' into PLG-352 PLG-392: Update navigation icons Merge branch 'master' into PLG-392 Merge branch 'master' into PLG-352 Merge branch 'master' into PLG-352 PLG-392: fix activity blurry icon in CE PLG-391: remove animation on DownloadIcon Merge branch 'master' into PLG-352 Merge branch 'master' into PLG-352 Merge branch 'master' into PLG-352 Merge branch 'master' into PLG-352 PLG-352: add a property to align menu item Merge branch 'master' into PLG-352 Merge branch 'master' into PLG-352 Merge branch 'master' into PLG-352 PLG-352: move the main entries build logic in a dedicated fonction fix front linter issue Adapt the product edition smoke test with the new PimNavigation PLG-352: fix menu items wrongly displayed Merge branch 'master' into PLG-352 Merge branch 'master' into PLG-352 PLG-352: remove duplicated line PLG-352: regroup navigation unit test fixtures in a helper PLG-352: fix unit test on navigation components PLG-352: fix sub nabvigation overflow PLG-352: add unit tests on navigation components PLG-352: fix sub menu container PLG-352: add Connect menu icon Merge branch 'master' into PLG-352 PLG-352: move Navigation components to front-packages/shared PLG-352: further code cleaning in the legacy modules PLG-352: move sub navigation dropdown into a dedicated component PLG-352: clean unused code in legacy modules PLG-352: fix sub navigation closed by default PLG-352: move sub navigation management into a dedicated component PLG-352: move menu form extensions into a dedicated folder PLG-352: working menu WIP - active entry and sub-entry PLG-352: WIP Merge pull request #14888 from akeneo/PLG-352

--- lib/components/Navigation/SubNavigationPanel/SubNavigationPanel.d.ts
+++ lib/components/Navigation/SubNavigationPanel/SubNavigationPanel.d.ts
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { ForwardRefExoticComponent, PropsWithoutRef, RefAttributes } from 'react';
 declare type SubNavigationPanelProps = {
     children?: React.ReactNode;
     isOpen?: boolean;
+    open: () => void;
+    close: () => void;
+    closeTitle?: string;
+    openTitle?: string;
 };
-declare const SubNavigationPanel: React.ForwardRefExoticComponent<SubNavigationPanelProps & React.RefAttributes<HTMLDivElement>>;
+declare type SubNavigationPanelCompoundType = ForwardRefExoticComponent<PropsWithoutRef<SubNavigationPanelProps> & RefAttributes<HTMLDivElement>> & {
+    Collapsed?: any;
+};
+declare const SubNavigationPanel: SubNavigationPanelCompoundType;
 export { SubNavigationPanel };
---
+++ lib/icons/ConnectIcon.d.ts
@@ -0,0 +1,3 @@
+import { IconProps } from './IconProps';
+declare const ConnectIcon: ({ title, size, color, ...props }: IconProps) => JSX.Element;
+export { ConnectIcon };
