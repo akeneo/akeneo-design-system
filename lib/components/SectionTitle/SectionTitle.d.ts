f9fc8fd5f724355531dd479f2e688c6a84823e23
akeneo <contact@akeneo.com>
2022-02-03 14:19:42 +0000

    Extract from https://github.com/akeneo/pim-community-dev/commit/d51ebb45fbdb9ae5d3c17b79bcb39b155b0f5e37 With commits: RAB-551: Add optional level prop on SectionTitle.Title (#16241) RAB-551: Add optional level prop on SectionTitle.Title (#16241)

--- lib/components/SectionTitle/SectionTitle.d.ts
+++ lib/components/SectionTitle/SectionTitle.d.ts
@@ -1,12 +1,15 @@
 import { HTMLAttributes, ReactNode } from 'react';
 import { Override } from '../../shared';
+declare type TitleProps = Override<HTMLAttributes<HTMLHeadingElement>, {
+    level?: 'primary' | 'secondary';
+}>;
 declare type SectionTitleProps = Override<HTMLAttributes<HTMLDivElement>, {
     sticky?: number;
     children?: ReactNode;
 }>;
 declare const SectionTitle: {
     ({ children, ...rest }: SectionTitleProps): JSX.Element;
-    Title: import("styled-components").StyledComponent<"h2", any, Record<string, unknown> & import("styled-components").ThemeProps<import("../../theme/theme").Theme>, never>;
+    Title: ({ level, ...rest }: TitleProps) => JSX.Element;
     Spacer: import("styled-components").StyledComponent<"div", any, {}, never>;
     Separator: import("styled-components").StyledComponent<"div", any, Record<string, unknown> & import("styled-components").ThemeProps<import("../../theme/theme").Theme>, never>;
     Information: import("styled-components").StyledComponent<"div", any, Record<string, unknown> & import("styled-components").ThemeProps<import("../../theme/theme").Theme>, never>;
