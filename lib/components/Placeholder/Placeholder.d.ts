409677412f44ef80a39c3e503b6245aa35afc195
akeneo <contact@akeneo.com>
2022-02-24 16:28:34 +0000

    Extract from https://github.com/akeneo/pim-community-dev/commit/8ed876c5258904dc6072ad91a2d6b8c817d9cd52 With commits: RAB-581: Add size prop on Placeholder component (#16380) RAB-581: Add size prop on Placeholder component (#16380)

--- lib/components/Placeholder/Placeholder.d.ts
+++ lib/components/Placeholder/Placeholder.d.ts
@@ -1,9 +1,11 @@
-import React, { ReactElement } from 'react';
+import { FC, HTMLAttributes, ReactElement } from 'react';
 import { Override } from '../../shared';
 import { IllustrationProps } from '../../illustrations/IllustrationProps';
-declare type PlaceholderProps = Override<React.HTMLAttributes<HTMLDivElement>, {
+declare type PlaceholderSize = 'default' | 'large';
+declare type PlaceholderProps = Override<HTMLAttributes<HTMLDivElement>, {
     title: string;
     illustration: ReactElement<IllustrationProps>;
+    size?: PlaceholderSize;
 }>;
-declare const Placeholder: React.FC<PlaceholderProps>;
+declare const Placeholder: FC<PlaceholderProps>;
 export { Placeholder };
