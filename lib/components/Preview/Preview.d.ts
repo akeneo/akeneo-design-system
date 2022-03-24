8de4cb3fb11fea7ec6d24c9c0bc2d081f8caad02
akeneo <contact@akeneo.com>
2022-03-24 13:49:04 +0000

    Extract from https://github.com/akeneo/pim-community-dev/commit/5b27b0cface98d64fb39187bfc256baf4d8c5a6c With commits: RAB-583: Add Preview.Row subcomponent & collapsable Preview (#16649) RAB-583: Add Preview.Row subcomponent & collapsable Preview (#16649)

--- lib/components/Preview/Preview.d.ts
+++ lib/components/Preview/Preview.d.ts
@@ -1,11 +1,28 @@
-import { ReactNode, HTMLAttributes } from 'react';
+import { ReactNode, HTMLAttributes, ReactElement } from 'react';
 import { Override } from '../../shared';
+import { IconButtonProps } from '../IconButton/IconButton';
+declare type RowProps = Override<HTMLAttributes<HTMLDivElement>, {
+    action?: ReactElement<IconButtonProps>;
+    children?: ReactNode;
+}>;
 declare type PreviewProps = Override<HTMLAttributes<HTMLDivElement>, {
     title: string;
     children?: ReactNode;
-}>;
+} & ({
+    isOpen: boolean;
+    collapseButtonLabel: string;
+    onCollapse: (isOpen: boolean) => void;
+} | {
+    isOpen?: undefined;
+    collapseButtonLabel?: undefined;
+    onCollapse?: undefined;
+})>;
 declare const Preview: {
-    ({ title, children, ...rest }: PreviewProps): JSX.Element;
+    ({ title, isOpen, collapseButtonLabel, onCollapse, children, ...rest }: PreviewProps): JSX.Element;
     Highlight: import("styled-components").StyledComponent<"span", any, Record<string, unknown> & import("styled-components").ThemeProps<import("../../theme/theme").Theme>, never>;
+    Row: {
+        ({ action, children }: RowProps): JSX.Element;
+        displayName: string;
+    };
 };
 export { Preview };
