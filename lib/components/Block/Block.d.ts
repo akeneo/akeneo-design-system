7f99c66339addf20247ac8106725d4b2ab58c9ee
akeneo <contact@akeneo.com>
2022-05-04 07:42:35 +0000

    Extract from https://github.com/akeneo/pim-community-dev/commit/d5817eb8131f46f9f8cdea6d0cc0434381f20355 With commits: RAB-713: Update Block component to handle collapse and multiple actions (#16929) RAB-713: Update Block component to handle collapse and multiple actions (#16929)

--- lib/components/Block/Block.d.ts
+++ lib/components/Block/Block.d.ts
@@ -1,18 +1,21 @@
 import React, { ReactNode } from 'react';
 import { Override } from '../../shared';
 declare type BlockProps = Override<Override<React.ButtonHTMLAttributes<HTMLButtonElement>, React.AnchorHTMLAttributes<HTMLAnchorElement>>, {
-    action?: ReactNode;
+    title: string;
+    actions?: ReactNode;
     ariaLabel?: string;
     ariaLabelledBy?: string;
     ariaDescribedBy?: string;
     children?: ReactNode;
-}>;
-declare const Block: React.ForwardRefExoticComponent<Omit<Override<React.ButtonHTMLAttributes<HTMLButtonElement>, React.AnchorHTMLAttributes<HTMLAnchorElement>>, "children" | "ariaLabel" | "ariaLabelledBy" | "ariaDescribedBy" | "action"> & {
-    action?: ReactNode;
-    ariaLabel?: string | undefined;
-    ariaLabelledBy?: string | undefined;
-    ariaDescribedBy?: string | undefined;
-    children?: ReactNode;
-} & React.RefAttributes<HTMLButtonElement>>;
+} & ({
+    isOpen: boolean;
+    collapseButtonLabel: string;
+    onCollapse: (isOpen: boolean) => void;
+} | {
+    isOpen?: undefined;
+    collapseButtonLabel?: undefined;
+    onCollapse?: undefined;
+})>;
+declare const Block: React.ForwardRefExoticComponent<BlockProps & React.RefAttributes<HTMLButtonElement>>;
 export { Block };
 export type { BlockProps };
