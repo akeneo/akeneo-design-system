ffd74f24a48bf140464e08512ef06299e1cc2b2a
akeneo <contact@akeneo.com>
2022-05-11 17:52:12 +0000

    Extract from https://github.com/akeneo/pim-community-dev/commit/944244a8e4221c03c16633144d81f29822e64cdd With commits: RAB-169: Add ability to override separator regex in TagsInput (#16991) RAB-169: Add ability to override separator regex in TagsInput (#16991)

--- lib/components/Input/TagInput/TagInput.d.ts
+++ lib/components/Input/TagInput/TagInput.d.ts
@@ -3,9 +3,10 @@ import { Override } from '../../../shared';
 import { InputProps } from '../common/InputProps';
 declare type TagInputProps = Override<Override<React.InputHTMLAttributes<HTMLInputElement>, InputProps<string[]>>, {
     value: string[];
-    onChange: (tags: string[]) => void;
     placeholder?: string;
     invalid?: boolean;
+    separators?: string[];
+    onChange: (tags: string[]) => void;
     onSubmit?: () => void;
 }>;
 declare const TagInput: FC<TagInputProps>;
