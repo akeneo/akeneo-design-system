7e51c45029788141043f16d7a2315caf0eae2505
akeneo <contact@akeneo.com>
2023-05-17 08:41:30 +0000

    Extract from https://github.com/akeneo/pim-community-dev/commit/7bd16b24838bf015742670ace15a3f0e66828688 With commits: RAB-1396: Add PAUSING and PAUSED status (#19691) RAB-1396: Add PAUSING and PAUSED status (#19691)

--- lib/components/Tooltip/Tooltip.d.ts
+++ lib/components/Tooltip/Tooltip.d.ts
@@ -1,9 +1,11 @@
-import React from 'react';
-declare const Tooltip: React.ForwardRefExoticComponent<Omit<React.HTMLAttributes<HTMLDivElement>, "width" | "direction" | "children" | "zIndex" | "iconSize"> & {
-    direction?: "left" | "right" | "bottom" | "top" | undefined;
-    zIndex?: number | undefined;
-    iconSize?: number | undefined;
-    children: React.ReactNode;
-    width?: number | undefined;
-} & React.RefAttributes<HTMLDivElement>>;
+import { HTMLAttributes, ReactNode } from 'react';
+import { Override } from '../../shared';
+declare type Direction = 'top' | 'right' | 'bottom' | 'left';
+declare type TooltipProps = Override<HTMLAttributes<HTMLDivElement>, {
+    direction?: Direction;
+    iconSize?: number;
+    children: ReactNode;
+    width?: number;
+}>;
+declare const Tooltip: ({ direction, iconSize, width, children, ...rest }: TooltipProps) => JSX.Element;
 export { Tooltip };
