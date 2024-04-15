9fe5f423e93ce743923fea751dddf0a1b7460998
akeneo <contact@akeneo.com>
2024-04-15 13:17:34 +0000

    Extract from e20ed5d6134a7299b47f20f7770d622e2cfb1cec

--- .github/workflows/bump-version.js
+++ .github/workflows/bump-version.js
@@ -4,15 +4,8 @@
 const {execSync} = require('child_process');
 const fs = require('fs');
 
-const filePath = process.argv[2];
-const externalPackageJson = process.argv[3];
-const commitMessagesFilepath = process.argv[4];
-
-if (typeof filePath === 'undefined') {
-    console.log(`usage: ${process.argv[0]} ${process.argv[1]} path_to_webhook_event.json external/package.json commit-messages.txt`);
-
-    return;
-}
+const externalPackageJson = process.argv[2];
+const commitMessagesFilepath = process.argv[3];
 
 // Let's define an enum for our bump levels
 const BumpLevelEnum = {
@@ -43,12 +36,9 @@ const getBumpNameFromBumpLevel = (bumpLevel) => {
     }
 }
 
-const rawdata = fs.readFileSync(filePath);
-const githubEvent = JSON.parse(rawdata);
-
-const lastCommits = (new String(execSync(`git rev-list ${githubEvent.before}..HEAD`))).split('\n');
+const lastCommits = (new String(execSync(`git rev-list ${process.env.BUMP_BASE_REVISION || 'master'}..HEAD`))).split('\n');
 const messages = lastCommits.map(commit => new String(execSync(`git log -n 1 --pretty=format:%s ${commit}`)));
-fs.writeFileSync(commitMessagesFilepath, `Extract from https://github.com/${githubEvent.repository.full_name}/commit/${githubEvent.after}\nWith commits:\n${messages.join('\n')}`);
+fs.writeFileSync(commitMessagesFilepath, `Extract from ${process.env.BUMP_REVISION}\nWith commits:\n${messages.join('\n')}`);
 
 const levelToBump = messages.reduce((currentBumpLevel, commit) => {
     const bumpLevel = getCommitMessageBumpLevel(commit);
